24a787fa8b603f1889539cb33266cd337efdd9d5
booth-w <personal@booth-w.co.uk>
2025-11-21 12:52:57 +0000

    Delete old grid game tests

--- tests/test.cs
+++ tests/test.cs
@@ -20,7 +20,6 @@ static partial class Test {
 		Enqueue();
 		Dequeue();
 		QueueClear();
-		CommandValidityChecker();
 	}
 
 	static private void GenerateTest<T>(string name, Func<T> testMethod, T expected) {
