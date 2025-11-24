60dbfd5bf62ec9a9ed84080d2f90900a7aad3329
booth-w <personal@booth-w.co.uk>
2025-11-24 15:23:41 +0000

    Change coordinate type from int array to struct

--- search/search.cs
+++ search/search.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-public partial class Search {
+public static partial class Search {
+	public struct Coordinate {
+		public int row;
+		public int col;
+	}
+
 	/// <summary>
 	/// North, East, South, West
 	/// </summary>
-	public int[][] neighbours = new int[4][] {
+	public static int[][] neighbours = new int[4][] {
 		new int[2] { -1, 0 },
 		new int[2] { 0, 1 },
 		new int[2] { 1, 0 },
@@ -15,20 +20,20 @@ public partial class Search {
 	/// <summary>
 	/// Dictionary of available search algorithms' names and their corresponding functions
 	/// </summary>
-	public var algorithms = new Dictionary<string, Func<int[,], int[], int[]>>() {
-		{"A*", this.AStar},
-		{"Dijkstra", this.Dijkstra},
-		{"Breadth-First Search", this.BFS},
-		{"Depth-First Search", this.DFS},
-		{"Best-First Search", this.BestFirstSearch},
-		{"Hill Climbing", this.HillClimb}
+	public static Dictionary<string, Func<int[,], int[], int[], LinkedList<Coordinate>>> algorithms = new Dictionary<string, Func<int[,], int[], int[], LinkedList<Coordinate>>>() {
+		{"A*", AStar},
+		{"Dijkstras", Dijkstras},
+		{"Breadth-First Search", BreadthFirst},
+		{"Depth-First Search", DepthFirst},
+		{"Best-First Search", BestFirst},
+		{"Hill Climbing", HillClimb}
 	};
 
 
 	/// <summary>
 	/// Calculate the Manhattan distance between two points
 	/// </summary>
-	public int Heuristic(int[] start, int[] end) {
+	public static int Heuristic(int[] start, int[] end) {
 		return Math.Abs(start[0] - end[0]) + Math.Abs(start[1] - end[1]);
 	}
 }
