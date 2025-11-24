f616ce192eb279efb6f7dfde61f17e3267e5b1ea
booth-w <personal@booth-w.co.uk>
2025-11-24 11:36:31 +0000

    Add neighbours

--- search/search.cs
+++ search/search.cs
@@ -2,6 +2,16 @@ using System;
 
 public partial class Search {
 	/// <summary>
+	/// North, East, South, West
+	/// </summary>
+	public int[][] neighbours = new int[4][] {
+		new int[2] { -1, 0 },
+		new int[2] { 0, 1 },
+		new int[2] { 1, 0 },
+		new int[2] { 0, -1 }
+	};
+
+	/// <summary>
 	/// Calculate the Manhattan distance between two points
 	/// </summary>
 	public int Heuristic(int[] start, int[] end) {
